17fa10bd0f5ff9b95df14f0a2c5ad7605e329c7d
flores030 <99411537+flores030@users.noreply.github.com>
2022-05-12 12:40:12 -0300

    Add files via upload

--- exer.js
+++ exer.js
@@ -1,32 +1,15 @@
-// let n = 100
-// for (i = 0; i < n; i ++){
-//  console.log(i + 1)
+// let nomes = ["Cebolinha", "Cascão", "Mônica", "Magali", "Penadinho"]
+// console.log(nomes)
+// console.log(nomes[0])
+// console.log(nomes[1])
+// console.log(nomes[2])
+// console.log(nomes[3])
+// console.log(nomes[4])
+// console.log(nomes.length)
 
-// }
+// let salarios = [2500, 2000, 3500, 2090, 1000, 1200, 1230, 3040]
+// console.log(salarios)
 
-// let n = 100
-// for (i = 0; i <= n; i ++){   
-//     if (i % 2 == 0) {
-//         console.log (i)
-//     }
- 
-
-// }
-
-// let media = 0 
-// let n = 5
-// for (let i = 0; i < n; i ++){
-//     let salario = Math.random () * 1000
-//     console.log ("Salário;", salario.toFixed(2))
-//     media += salario
-// }
-// media = media / n 
-// console.log ("Média",media.toFixed(2))
-
-// let salario = 150000
-// for (let i = 0; i < 5; i++){
-//     salario = salario /2
-//     console.log ('Salário:', salario)
-
-// }
+// let booleanos= [true, false, true, false, true, false]
+// console.log(booleanos)
 
